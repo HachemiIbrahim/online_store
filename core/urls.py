a2cddf4562bddfac7f32d92e6b8bfffcc8ef1923
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-12-31 23:51:21 +0100

    login form

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 
 from . import views
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView, name="login"),
 ]
