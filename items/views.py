fea35e5eea2521d36eb30d57d300c4c8a9bcdff6
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2025-01-05 23:23:36 +0100

    edit item

--- items/views.py
+++ items/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 from .models import Item
 
 
@@ -21,21 +21,46 @@ def detail(request, pk):
 def new(request):
     if request.method == "POST":
         form = NewItemForm(request.POST, request.FILES)
-        item = form.save(commit=False)
-        item.created_by = request.user
-        item.save()
 
-        return redirect(
-            "items:detail",
-            pk=item.id,
-        )
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect("item:detail", pk=item.id)
     else:
-        form = NewItemForm
+        form = NewItemForm()
+
     return render(
         request,
         "items/form.html",
         {
             "form": form,
+            "title": "New item",
+        },
+    )
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == "POST":
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(
+        request,
+        "items/form.html",
+        {
+            "form": form,
+            "title": "Edit item",
         },
     )
 
