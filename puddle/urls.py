ab9ad11320f369b9d1d7c3adbea14013956fe415
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2025-01-25 19:37:42 +0100

    communication

--- puddle/urls.py
+++ puddle/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("items/", include("items.urls")),
     path("dashboard/", include("dashboard.urls")),
     path("admin/", admin.site.urls),
+    path("inbox/", include("communication.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
